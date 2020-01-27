8e3207df8c75df8167201bc5ab6d7f8fa7f7172f
Vindicator Rhaena <vrhaena@at-q9.21-school.ru>
2020-01-27 21:50:19 +0300

    Add some new dislocation func

--- srcs/dislocation.c
+++ srcs/dislocation.c
@@ -6,7 +6,7 @@
 /*   By: oearlene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 22:51:02 by oearlene          #+#    #+#             */
-/*   Updated: 2020/01/25 23:35:56 by oearlene         ###   ########.fr       */
+/*   Updated: 2020/01/27 21:36:48 by vrhaena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void		dislocate_x(t_piece *piece, int n)
 	i = 0;
 	while (i <= 3)
 	{
-		piece->x[i] += n;
+		piece->x[i] -= n;
 		i++;
 	}
 }
@@ -31,7 +31,7 @@ void		dislocate_y(t_piece *piece, int n)
 	i = 0;
 	while (i <= 3)
 	{
-		piece->y[i] += n;
+		piece->y[i] -= n;
 		i++;
 	}
 }
@@ -39,15 +39,23 @@ void		dislocate_y(t_piece *piece, int n)
 t_piece		*in_order(t_piece *piece)
 {
 	int		i;
+	int		x_min;
+	int 	y_min;
 
 	i = 0;
+	x_min = 3;
+	y_min = 3;
 	while (i <= 3)
 	{
-		if (piece->x[0] != 0)
-			dislocate_x(piece, -1);
-		if (piece->y[0] != 0)
-			dislocate_y(piece, -1);
+		if (piece->x[i] < x_min)
+			x_min = piece->x[i];
+		if (piece->y[i] < y_min)
+			y_min = piece->y[i];
 		i++;
 	}
+	if (piece->x[0] != 0)
+		dislocate_x(piece, x_min);
+	if (piece->y[0] != 0)
+		dislocate_y(piece, y_min);
 	return (piece);
 }
